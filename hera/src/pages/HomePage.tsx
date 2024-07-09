9610a47cd067f187f4d20b764e937194d2507fc1
kabottin0 <gc.eforge@gmail.com>
2024-07-09 14:28:10 +0200

    edit homepage add href

--- hera/src/pages/HomePage.tsx
+++ hera/src/pages/HomePage.tsx
@@ -31,17 +31,24 @@ const HomePage = () => {
         <div className="text-center w-3/12"></div>
         <div className="text-center w-6/12 ">
           <div className="flex">
-            <div className="w-3/12 pb-10 mr-2 p-1 bg-white">
+            <div className="w-3/12  mr-2 p-1 bg-white">
               <img src="https://via.placeholder.com/150" alt="Immagine 1" className="w-full" />
+              <p> nome villa 1</p>
             </div>
-            <div className="w-3/12 pb-10 mx-2 p-1 bg-white">
+            <div className="w-3/12  mx-2 p-1 bg-white">
               <img src="https://via.placeholder.com/150" alt="Immagine 1" className="w-full" />
+              <p> nome sala 1</p>
+
             </div>
-            <div className="w-3/12 pb-10 mx-2 p-1 bg-white">
+            <div className="w-3/12  mx-2 p-1 bg-white">
               <img src="https://via.placeholder.com/150" alt="Immagine 1" className="w-full" />
+              <p> nome dimora 1</p>
+
             </div>
-            <div className="w-3/12 pb-10 ml-2 p-1 bg-white">
+            <div className="w-3/12 pb-2 ml-2 p-1 bg-white">
               <img src="https://via.placeholder.com/150" alt="Immagine 1" className="w-full" />
+              <p> nome catering 1</p>
+
             </div>
           </div>
         </div>
@@ -60,32 +67,32 @@ const HomePage = () => {
       </div>
 
       <div className="flex justify-around items-center pt-8">
-        <div className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
+        <a href="/ville" className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
           <div className="absolute inset-0 flex items-center justify-center group-hover:bg-opacity-50 transition duration-300">
             <span className="text-white">VILLE</span>
           </div>
           <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-100 transition duration-300" style={{ backgroundImage: `url(https://via.placeholder.com/300)` }}></div>
-        </div>
-        <div className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
+        </a>
+        <a href="/Sale" className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
           <div className="absolute inset-0 flex items-center justify-center group-hover:bg-opacity-50 transition duration-300">
             <span className="text-white">SALE</span>
           </div>
           <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-100 transition duration-300" style={{ backgroundImage: `url(https://via.placeholder.com/300)` }}></div>
-        </div>
-        <div className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
+        </a>
+        <a href="/dimore" className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
           <div className="absolute inset-0 flex items-center justify-center group-hover:bg-opacity-50 transition duration-300">
             <span className="text-white">DIMORE</span>
           </div>
           <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-100 transition duration-300" style={{ backgroundImage: `url(https://via.placeholder.com/300)` }}></div>
-        </div>
-        <div className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
+        </a>
+        <a href="/catering" className="relative group w-32 h-32 rounded-full border-4 border-white overflow-hidden">
           <div className="absolute inset-0 flex items-center justify-center group-hover:bg-opacity-50 transition duration-300">
-            <span className="text-white">CATERING</span>
+            <span className="text-white">Catering</span>
           </div>
           <div className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-100 transition duration-300" style={{ backgroundImage: `url(https://via.placeholder.com/300)` }}></div>
-        </div>
+        </a>
       </div>
-    
+
       <div className="max-w-4xl mx-auto my-8 p-4 bg-white rounded-lg">
         <h2 className="text-lg font-bold mb-4" style={{ color: '#D4AF37' }}>Q & A</h2>
         {questions.map((item, index) => (
@@ -102,7 +109,7 @@ const HomePage = () => {
           </div>
         ))}
       </div>
-      
+
     </>
   );
 };
